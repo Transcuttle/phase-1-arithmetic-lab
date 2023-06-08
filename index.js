01c6cf86dfe61c4d06c6ff18692ea06e9a6b6296
George <georgefbrockman@gmail.com>
2023-06-08 16:24:25 -0500

    defines variables

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const num3 = 8;
+const num4 = 12;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 20) + 1;
+
+const mod = num4 % num3;
+
+const max = Math.max(5, 20, 6, 18, 16, 3);
